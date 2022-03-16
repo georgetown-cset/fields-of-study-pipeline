74b1ac9ce2b55bc888dc301af4d856aa00e1aff2
James Dunham <james@jdunham.io>
2022-03-16 19:25:44 -0400

    Add some fasttext tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,48 @@
 import contextlib
+import gzip
 import json
 import os
+import pickle
 from pathlib import Path
 
 import fasttext
 import fasttext.util
 import gensim
+import pandas as pd
 import pytest
 
+from fos.settings import CORPUS_DIR, ASSETS_DIR
 from fos.util import preprocess
 
-ASSETS_DIR = Path(__file__).parent / 'assets'
+TEST_ASSETS_DIR = Path(__file__).parent / 'assets'
 
 
 @pytest.fixture
 def texts():
     """Load example texts."""
-    with open(ASSETS_DIR / 'texts.json', 'rt') as f:
+    with open(TEST_ASSETS_DIR / 'texts.json', 'rt') as f:
         return json.load(f)
+
+
+@pytest.fixture
+def meta() -> pd.DataFrame:
+    """Load field metadata."""
+    with gzip.open(ASSETS_DIR / 'fields/fos.pkl.gz', 'rb') as f:
+        df = pickle.load(f)
+    return df
+
+
+@pytest.fixture
+def mag_outputs():
+    """Load MAG outputs."""
+    with open(CORPUS_DIR / 'mag-output.jsonl', 'rt') as f:
+        return [json.loads(line) for line in f]
 
 
 @pytest.fixture
 def preprocessed_texts():
     """Load example texts and preprocess them."""
-    with open(ASSETS_DIR / 'texts.json', 'rt') as f:
+    with open(TEST_ASSETS_DIR / 'texts.json', 'rt') as f:
         texts_ = json.load(f)
         return {k: preprocess(v) for k, v in texts_.items()}
 
@@ -34,7 +53,7 @@ def vanilla_en_fasttext():
 
     On first run, this will download the model to the tests/assets directory.
     """
-    with working_directory(ASSETS_DIR.absolute()):
+    with working_directory(TEST_ASSETS_DIR.absolute()):
         fasttext.util.download_model('en', if_exists='ignore')  # English
         return fasttext.load_model('cc.en.300.bin')
 
@@ -45,7 +64,7 @@ def vanilla_en_word2vec():
 
     On first run, this will download the model to the tests/assets directory.
     """
-    with working_directory(ASSETS_DIR.absolute()):
+    with working_directory(TEST_ASSETS_DIR.absolute()):
         return gensim.utils.downloader.api.load('word2vec-google-news-300')
 
 
---
+++ tests/test_fasttext.py
@@ -0,0 +1,43 @@
+import json
+
+import numpy as np
+from fasttext.FastText import _FastText, load_model
+
+from fos.model import FieldModel
+from fos.settings import EN_FASTTEXT_PATH, EN_FIELD_TEXT
+from fos.util import norm
+from fos.vectors import load_fasttext
+
+
+def test_load_model():
+    """Our wrapper for loading a model should be equivalent to using fasttext's load_model()."""
+    vanilla_load = load_model(str(EN_FASTTEXT_PATH))
+    assert isinstance(vanilla_load, _FastText)
+    wrapped_load = load_fasttext("en")
+    assert isinstance(wrapped_load, _FastText)
+    assert len(wrapped_load.get_words()) and vanilla_load.get_words() == wrapped_load.get_words()
+
+
+def test_field_embeddings(meta):
+    """FastText field embeddings loaded by FieldModel should be the same as those from embedding the field text."""
+    ft = load_model(str(EN_FASTTEXT_PATH))
+    field_model = FieldModel("en")
+    field_text = json.load(open(EN_FIELD_TEXT, "rt"))
+    field_index = np.array(field_model.index)
+    rows = set()
+    for field, text in field_text.items():
+        # Embed the field text with fasttext's get_sentence_vector
+        ft_vector = ft.get_sentence_vector(text)
+        # Look up the field ID associated with the field text
+        field_id = meta.loc[meta.display_name == field].index[0]
+        # Find the row of the field matrix that corresponds with the field ID
+        field_model_row = np.where(field_id == field_index)[0][0]
+        rows.add(field_model_row)
+        # Slice into the field matrix to get the field vector
+        field_model_vector = field_model.field_fasttext.index[field_model_row, :]
+        # The normed vector from applying get_sentence_vector to the field text should be the same as the corresponding
+        # row in the field matrix
+        assert np.array_equiv(norm(ft_vector), field_model_vector)
+    # And by iterating over all the field text, we iterated over all the rows of the field matrix
+    assert len(rows) == len(field_text) == field_model.field_fasttext.index.shape[0]
+    assert len(field_index) == field_model.field_fasttext.index.shape[0]
