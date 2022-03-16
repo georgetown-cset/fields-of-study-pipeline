c3a33814263244a76b3e5a84d88d4ec6612ce689
James Dunham <james@jdunham.io>
2022-03-16 19:47:40 -0400

    Reorganize preprocessing tests

--- fos/util.py
+++ fos/util.py
@@ -24,19 +24,6 @@ LONE_NUMBERS = re.compile(r'\b\d+\b')
 NONBREAKING_SPACE = re.compile(r"[^\S\n\v]+")
 
 
-def test_replace():
-    assert 'QUICK BROWN FOX?'.translate(TO_CLEAN_LOWER) == 'quick brown fox'
-    assert 'A 0e!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~]'.translate(TO_CLEAN_LOWER) == 'a 0e'
-    assert '产业组织理论'.translate(TO_CLEAN_LOWER) == '产业组织理论'
-    assert '\r\n'.translate(TO_CLEAN_LOWER) == '  '
-    assert LONE_NUMBERS.sub('', 'X11 11') == 'X11 '
-
-
-def test_preprocess():
-    assert preprocess(' QUICK BROWN FOX?', 'en') == 'quick brown fox'
-    assert preprocess('产业组织理论', 'zh') == '产业组织理论'
-
-
 def preprocess(text, lang='en'):
     if lang == 'en':
         # For English, remove everything but alpha, spaces, and digits; also remove lone numbers
