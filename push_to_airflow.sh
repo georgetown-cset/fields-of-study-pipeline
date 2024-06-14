bcec53e6c69690c1064729c645c30b0872b3036b
James Dunham <james@jdunham.io>
2024-06-14 18:17:44 -0400

    Push the dvc config but not the dvc cache to the VM via GCS

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -1,6 +1,7 @@
 gsutil -m cp new_fields_of_study_dag.py gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/
 gsutil -m rm -r gs://airflow-data-exchange/fields_of_study_v2/*
-gsutil -m cp -r ../fields-of-study-pipeline/{assets,.dvc,.git,fos,scripts,sql,requirements.txt} gs://airflow-data-exchange/fields_of_study_v2/fields-of-study-pipeline/
+gsutil -m cp -r ../fields-of-study-pipeline/{assets,.git,fos,scripts,sql,requirements.txt} gs://airflow-data-exchange/fields_of_study_v2/fields-of-study-pipeline/
+gsutil -m cp -r ../fields-of-study-pipeline/.dvc/config gs://airflow-data-exchange/fields_of_study_v2/fields-of-study-pipeline/.dvc/config
 gsutil -m rm gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/fields_of_study_v2/*
 gsutil -m cp sql/* gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/fields_of_study_v2/
 gsutil -m cp schemas/* gs://airflow-data-exchange/fields_of_study_v2/schemas/
