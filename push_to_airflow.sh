2a960fda47b765ab05da65e7114dbe3f74b28305
James Dunham <jamesdunham@users.noreply.github.com>
2025-01-28 11:35:31 -0500

    Tidy up

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -1,9 +1,21 @@
-gsutil -m cp new_fields_of_study_dag.py gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/
-gsutil -m rm -r gs://airflow-data-exchange/fields_of_study_v2/*
-gsutil -m cp -r ../fields-of-study-pipeline/{assets,.git,fos,scripts,sql,requirements.txt} gs://airflow-data-exchange/fields_of_study_v2/fields-of-study-pipeline/
-gsutil -m cp -r ../fields-of-study-pipeline/.dvc/config gs://airflow-data-exchange/fields_of_study_v2/fields-of-study-pipeline/.dvc/config
-gsutil -m rm gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/fields_of_study_v2/*
-gsutil -m cp sql/* gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/fields_of_study_v2/
-gsutil -m cp schemas/* gs://airflow-data-exchange/fields_of_study_v2/schemas/
-gsutil -m cp schemas/* gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/schemas/fields_of_study_v2/
-gsutil -m cp query_sequence.txt gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sequences/fields_of_study_v2/
+AIRFLOW_PREFIX="gs://us-east1-production-cc2-202-b42a7a54-bucket/dags"
+DATA_PREFIX="gs://airflow-data-exchange/fields_of_study_v2"
+
+gsutil -m cp new_fields_of_study_dag.py $AIRFLOW_PREFIX/
+gsutil -m rm -r $DATA_PREFIX/*
+gsutil -m cp -r ./{fos,scripts,sql,requirements.txt} $DATA_PREFIX/fields-of-study-pipeline/
+gsutil -m cp schemas/* $DATA_PREFIX/fields-of-study-pipeline/schemas/
+
+# Files directly under the assets directory
+gsutil -m cp assets/* $DATA_PREFIX/fields-of-study-pipeline/assets/
+# And also those in assets/fields, but not e.g. assets/corpus etc
+gsutil -m cp -r assets/{fields,scientific-lit-embeddings} $DATA_PREFIX/fields-of-study-pipeline/assets/
+
+# We need the dvc config so we can pull from the GCS remote
+gsutil -m cp -r ../fields-of-study-pipeline/.dvc/config $DATA_PREFIX/fields-of-study-pipeline/.dvc/config
+
+gsutil -m rm $AIRFLOW_PREFIX/sql/fields_of_study_v2/*
+gsutil -m cp sql/* $AIRFLOW_PREFIX/sql/fields_of_study_v2/
+
+gsutil -m cp schemas/* $AIRFLOW_PREFIX/schemas/fields_of_study_v2/
+gsutil -m cp query_sequence.txt $AIRFLOW_PREFIX/sequences/fields_of_study_v2/
