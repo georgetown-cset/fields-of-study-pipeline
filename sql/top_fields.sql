7a99b941d8e0771b51bab9c9f10ac1a029ed8d7f
James Dunham <jamesdunham@users.noreply.github.com>
2025-05-14 06:27:44 -0400

    Update top_fields.sql for constrained scoring; add some extra convenience columns.
    We can simplify this query a great deal now that constraints are applied earlier, during scoring.

--- sql/top_fields.sql
+++ sql/top_fields.sql
@@ -1,90 +1,53 @@
-with unnested as (
-  select
-    merged_id,
-    field.name,
-    field.score as field_score
-  from {{staging_dataset}}.field_scores, unnest(fields) as field
-),
+create or replace table staging_fields_of_study_v2.top_fields
 
-field_ranks as (
-  select
-    merged_id,
-    trim(unnested.name) as name,
-    level as field_level,
-    field_score,
-    row_number() over (partition by merged_id, level order by field_score desc) as field_rank
-  from unnested
-  inner join {{staging_dataset}}.field_meta on trim(unnested.name) = trim(field_meta.name)
-),
+cluster by
+  top_l0,
+  top_l1,
+  top_l2,
+  top_l3
 
-top_l0_fields as (
-  select
-    field_ranks.*,
-    field_hierarchy.child_display_name as l1_child_name
-  from field_ranks
-  inner join {{staging_dataset}}.field_hierarchy
-    on trim(field_ranks.name) = trim(field_hierarchy.display_name)
-  where
-    field_level = 0
-    and field_rank <= 2
-),
+as (
 
-top_l1_fields as (
-  select *
-  from field_ranks
-  where
-    field_level = 1
-    and field_rank <= 3
-),
+  with unnested as (
+    select
+      merged_id,
+      field.name,
+      field.score,
+    from {{staging_dataset}}.field_scores, unnest(fields) as field
+    where
+      -- Omit zero scores
+      field.score > 0.0
+  ),
 
-l2_candidates as (
-  select
-    top_l1_fields.merged_id,
-    field_ranks.name,
-    field_ranks.field_level,  # This will always be 2 or 3, because of the series of inner joins below
-    field_ranks.field_score,
-    # Rerank the L2s that are children of the top L1s
-    row_number() over (partition by top_l1_fields.merged_id, field_ranks.field_level order by field_ranks.field_score desc) as field_rank
-  from top_l1_fields
-  # Get the L2 children of the top L1 fields; this is just a taxonomy lookup
-  inner join {{staging_dataset}}.field_hierarchy l2_children
-    on trim(top_l1_fields.name) = trim(l2_children.display_name)
-  # Also get top-L0 fields with their L1 children to restrict against
-  inner join top_l0_fields
-    on trim(top_l1_fields.name) = trim(top_l0_fields.l1_child_name)
-  # Get the scores for the children of the top L1 fields
-  inner join field_ranks
-    # For each merged_id ...
-    on field_ranks.merged_id = top_l1_fields.merged_id
-    # We want the scores of the fields that are L2 or L3 children of the top L1 fields
-    and trim(field_ranks.name) = trim(l2_children.child_display_name)
-),
-
-# After re-ranking the subset of L2 fields, we can restrict on the top L2s in the subset
-top_l2_fields as (
-  select *
-  from l2_candidates
-  where field_rank = 1
-  order by merged_id, field_level, field_rank
-),
-
-combined as (
-  # Combine the two sets of levels
-  select *
-  from field_ranks
-  where field_level between 0 and 1
-  and field_rank = 1
-  union all
-  select * from top_l2_fields
-)
-
-select
-  merged_id,
-  array_agg(
-    struct(
+  ranked as (
+    select
+      merged_id,
       name,
-      field_level as level,
-      field_score as score)
-    order by field_level) fields
-from combined
-group by merged_id
+      level,
+      score,
+      row_number() over (partition by merged_id, level order by score desc) as field_rank
+    from unnested
+    inner join {{staging_dataset}}.field_meta using(name)
+  )
+
+  select
+    merged_id,
+    array_agg(
+      struct(
+        name,
+        level,
+        score)
+      order by level, score desc) as fields,
+      any_value(if(level = 0 and field_rank = 0, name, null)) top_l0,
+      any_value(if(level = 1 and field_rank = 0, name, null)) top_l1,
+      any_value(if(level = 2 and field_rank = 0, name, null)) top_l2,
+      any_value(if(level = 3 and field_rank = 0, name, null)) top_l3,
+      array_agg(if(level = 0, name, null) ignore nulls order by score desc) top_3_l0,
+      array_agg(if(level = 1, name, null) ignore nulls order by score desc) top_3_l1,
+      array_agg(if(level = 2, name, null) ignore nulls order by score desc) top_3_l2,
+      array_agg(if(level = 3, name, null) ignore nulls order by score desc) top_3_l3,
+  from ranked
+  where field_rank <= 3
+  group by merged_id
+
+)
