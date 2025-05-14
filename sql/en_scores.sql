428e31c4cf6b97d5d0de2bcdddfc797a48041ab7
James Dunham <jamesdunham@users.noreply.github.com>
2025-05-14 06:15:15 -0400

    Correct casing for excluded fields

--- sql/en_scores.sql
+++ sql/en_scores.sql
@@ -1,5 +1,5 @@
 -- Merge any new scores with the old ones in the production dataset; if we have scores for
--- an id in both tables (which can happen if the text for that id changed), take the new --score
+-- an id in both tables (which can happen if the text for that id changed), take the new score
 with in_lang_ids as (
   select
     merged_id,
@@ -66,6 +66,6 @@ select
       field.score
     from unnest(fields) AS field
     -- Exclude fields we aren't satisfied with, after review
-    where field.name not in ('Process management', 'Access control', 'Social engineering')
+    where field.name not in ('Process Management', 'Access Control')
   ) as fields
 from combined_scores
