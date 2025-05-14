bdddfff2ea4004dcba6f80b2bfd91829d6274f41
James Dunham <jamesdunham@users.noreply.github.com>
2025-05-14 06:15:44 -0400

    Correct struct field order

--- sql/field_scores.sql
+++ sql/field_scores.sql
@@ -12,17 +12,17 @@ with merged as (
     merged_id,
     -- We unnest and nest again so that the structs match; imputed_scores has more nested fields
     array_agg(struct(
-      field.name,
-      field.score
+      field.score,
+      field.name
       )) as fields,
     true as is_imputed
   from {{staging_dataset}}.imputed_scores, unnest(fields) as field
   group by merged_id
 )
 
-select
-  *
-from
-  merged
-where
-  merged_id in (select merged_id from literature.sources)
+select *
+from merged
+where merged_id in (
+  select merged_id
+  from literature.sources
+)
